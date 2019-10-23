6e8d2b759671171977b9b912cf5e5820e7c1273c
Tomek <owerko@gmail.com>
2019-10-23 08:46:19 +0200

    added modis example processing

---
+++ MODIS_Terra/modis_example_processing.py
@@ -0,0 +1,43 @@
+import numpy as np
+from osgeo import gdal
+import matplotlib.pyplot as plt
+
+example_file = 'data/MOD11B3.A2016214.h19v03.006.2016286174218.hdf'
+# example_file = 'data/MOD11B3.A2016214.h19v04.006.2016286174221.hdf'
+
+dataset = gdal.Open(example_file)
+subdatasets = dataset.GetSubDatasets()
+for subdataset in subdatasets:
+    print(subdataset[1])
+
+scale = 0.02
+lst_day = gdal.Open(subdatasets[0][0])
+lst_night = gdal.Open(subdatasets[4][0])
+
+array_lst_day = lst_day.ReadAsArray().astype(np.float32) * scale
+array_lst_night = lst_night.ReadAsArray().astype(np.float32) * scale
+
+print('Max LST during the day:', np.max(array_lst_day))
+print('Max LST during the night:',  np.max(array_lst_night))
+celsius_lst_day = array_lst_day - 273.15
+celsius_lst_day[celsius_lst_day < 0] = np.nan
+celsius_lst_night = array_lst_night - 273.15
+celsius_lst_night[celsius_lst_night < 0] = np.nan
+
+plt.figure(figsize=(10,10))
+imgplot = plt.imshow(celsius_lst_day)
+imgplot.set_cmap('plasma')
+plt.colorbar()
+plt.title('LST daytime')
+plt.show()
+
+plt.figure(figsize=(10,10))
+imgplot = plt.imshow(celsius_lst_night)
+imgplot.set_cmap('plasma')
+plt.colorbar()
+plt.title('LST nighttime')
+plt.show()
+
+del lst_day
+del lst_night
+del dataset
---
+++ MODIS_Terra/modis_preprocessing.py
@@ -0,0 +1,43 @@
+import subprocess
+import matplotlib.pyplot as plt
+import numpy as np
+from osgeo import gdal
+# from osgeo import gdalplugins
+
+# test_image = 'raw/MOD11B3.A2000153.h19v03.006.2015160142129.hdf'
+test_image = 'data/MOD11B3.A2016214.h19v03.006.2016286174218.hdf'
+vector = 'vector/warszawa.shp'
+# vector = 'vector/krakow_krakowskie.shp'
+lst_output = 'test_lst.tiff'
+
+# Read image and process it
+
+dataset = gdal.Open(test_image)
+subdatasets = dataset.GetSubDatasets()
+del dataset
+
+lst_day = subdatasets[0][0]
+subprocess.call(['gdalwarp',
+                 '-cutline',
+                 vector,
+                 '-crop_to_cutline',
+                 '-dstalpha',
+                 lst_day,
+                 lst_output])
+del lst_day
+
+# Check output
+scaling_factor = 0.02
+lst_day = gdal.Open(lst_output)
+array_lst_day = (lst_day.ReadAsArray().astype(np.float32))[0] * scaling_factor
+del lst_day
+
+celsius_lst_day = array_lst_day - 273.15
+celsius_lst_day[celsius_lst_day < 0] = np.nan
+
+plt.figure(figsize=(10,10))
+imgplot = plt.imshow(celsius_lst_day)
+imgplot.set_cmap('plasma')
+plt.colorbar()
+plt.title('LST daytime for Warsaw')
+plt.show()
